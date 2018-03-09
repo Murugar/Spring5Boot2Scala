bdf3f08718b1644944d6ed3fb108199a372278f5
Davanon <davanon2014@gmail.com>
2018-03-09 09:10:52 +0530

    Sec

--- src/main/scala/com/iqmsoft/scalaspring/SampleScalaController.scala
+++ src/main/scala/com/iqmsoft/scalaspring/SampleScalaController.scala
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
 @Controller
 @EnableAutoConfiguration class SampleScalaController {
   @RequestMapping(Array("/"))
-  @ResponseBody private[scalaspring] def home = "Hello World!"
+  @ResponseBody private[scalaspring] def home = "Hello World from Spring Boot2 Scala!"
 }
